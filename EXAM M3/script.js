563919b9fb50ea45065ff157b7f2a4e7dda90243
giuliamrc <giuliamarcucci2004@gmail.com>
2025-03-10 11:09:04 +0100

    EXAM M3

---
+++ EXAM M3/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBeetween:14,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+  
